5723b47b284d700940ae06d4524a03f96f93edba
Parth Patel <Parth.Patel@dal.ca>
2019-02-10 23:15:13 -0400

    Update Lab5task4.py

---
+++ Lab5task4.py
@@ -0,0 +1,16 @@
+from time import gmtime, strftime
+from datetime import datetime,timedelta
+class Time():
+    def __init__(self,t1,t2):
+         self.t1 = t1
+         self.t2 = t2
+
+    def is_after(self):
+         return self.t1 < self.t2
+
+y = datetime.now() - timedelta(days=1)
+z = datetime.now()
+print(y)
+print(z)
+myobj = Time(z,y)
+print(myobj.is_after())
